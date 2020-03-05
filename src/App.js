99e3862ed9c4a6141d59c75c8b9a9e56b85e863a
Xu Gao <xgao@microsoft.com>
2020-03-04 17:42:29 -0800

    try alias

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "office-ui-fabric-react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button content="Connect matrix" loader="Program alarm" />
     </div>
   );
 }
